551c37858e3e61713168d7c8a4fbdbc0f3f4b42f
tgloeggl <tgloeggl@uos.de>
2015-01-19 11:07:17 +0100

    enable trails in ohn-plugin

---
+++ controllers/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * File - description
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * version 3 as published by the Free Software Foundation.
+ *
+ * @author      Till Glöggler <tgloeggl@uos.de>
+ * @license     https://www.gnu.org/licenses/agpl-3.0.html AGPL version 3
+ */
+
+class IndexController extends StudipController
+{
+    function before_filter(&$action, &$args)
+    {
+        parent::before_filter($action, $args);
+
+        $this->set_layout($GLOBALS['template_factory']->open('layouts/base'));
+    }
+
+    public function impressum_action()
+    {
+        
+    }
+}
