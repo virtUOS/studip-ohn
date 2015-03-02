858315bfbf2d562f2ee5a96ce4beb7f1c7ac6a3d
lkoehler <lucas.koehler@hotmail.de>
2015-03-02 14:47:41 +0100

    Styled Dashboard

--- assets/application.js
+++ assets/application.js
@@ -4,6 +4,8 @@
 	    $(".user_dropdown").click(function() {
 		    $(".user_menu ul").toggle();
 	    });
+	    
+	    $(".start-widgetcontainer .portal-widget-list .ui-widget-content article img").wrap("<div class='widget_image_wrapper'></div>");
 		});
     
 
