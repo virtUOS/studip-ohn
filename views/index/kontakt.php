589d5013fdb0381247949481d6c597ac89a34ee7
asudau <sudau@elan-ev.de>
2017-11-02 09:56:45 +0100

    neue support mailadressen

--- views/index/kontakt.php
+++ views/index/kontakt.php
@@ -1,8 +1,8 @@
 <div id="mooc-kontakt">
 <h1>Kontakt</h1>
 <h2>Technische Fragen</h2>
-<p>Bei technischen Problemen wenden Sie sich an <a href="mailto:technik@ohn-kursportal.de" target="_blank">technik@ohn-kursportal.de</a>.
+<p>Bei technischen Problemen wenden Sie sich an <a href="mailto:ohn_kursportal_technik@elan-ev.de" target="_blank">ohn_kursportal_technik@elan-ev.de</a>.
 </p>
 <h2>Sonstige Fragen</h2>
-<p>Bei allen sonstigen Fragen wenden Sie sich an <a href="mailto:kontakt@ohn-kursportal.de" target="_blank">kontakt@ohn-kursportal.de</a>.</p>
+<p>Bei allen sonstigen Fragen wenden Sie sich an <a href="mailto:ohn_kursportal_kontakt@elan-ev.de" target="_blank">ohn_kursportal_kontakt@elan-ev.de</a>.</p>
 </div>
